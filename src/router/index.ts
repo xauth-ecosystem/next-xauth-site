13b2b323342607c6ce8512b70968b9f8c5963623
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-12-30 17:33:46 +0200

    feat: Configure Vue Router

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/download',
+      name: 'download',
+      component: () => import('../views/DownloadPage.vue'),
+    },
+    {
+      path: '/support',
+      name: 'support',
+      component: () => import('../views/SupportPage.vue'),
+    },
+    {
+      path: '/wiki',
+      name: 'wiki',
+      component: () => import('../views/WikiPage.vue'),
+    },
+  ],
+})
+
+export default router
